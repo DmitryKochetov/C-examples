bb75dc3aa6dacd67ac35facfd3a9c4894fafed1c
Dmitry <kochetov.dmitry@mail.ru>
2022-07-07 15:34:31 +0300

    добавлены все папки

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your name: ");
+string userName = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(userName);
